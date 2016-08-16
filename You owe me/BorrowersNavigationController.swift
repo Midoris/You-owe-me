7f1f472b5022f39d00204cc5a0f169c339bff701
Iablonskyi Ievgenii <eugen_yablonski@yahoo.com>
2016-08-16 13:55:29 +0700

    coloring almost done

---
+++ You owe me/BorrowersNavigationController.swift
@@ -0,0 +1,35 @@
+//
+//  BorrowersNavigationController.swift
+//  You owe me
+//
+//  Created by тигренок  on 16/08/2016.
+//  Copyright © 2016 Iablonskyi Ievgenii. All rights reserved.
+//
+
+import UIKit
+
+class BorrowersNavigationController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
